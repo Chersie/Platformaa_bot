c91ce7a6a294e777fdc980811f9195f920ea52c9
Chersie225 <ivansupr225@gmail.com>
2021-08-12 19:18:33 +0300

    vk api kinda finished

--- utils/vk_api/tasks.py
+++ utils/vk_api/tasks.py
@@ -1,27 +1,25 @@
 from vkwave_api import API, run
 
 
-async def are_friends(follower_token, user_id):
-    vk_api = API(follower_token)
-    api = vk_api.get_api()
-    follower_id = await api.users.get(return_raw_response=True)
-    follower_id = follower_id["response"][0]["id"]
-    are_friends_raw = await api.friends.are_friends(user_ids=(follower_id, user_id),
-                                                    return_raw_response=True)
-    friend_status = are_friends_raw["response"][0]["friend_status"]
-    await vk_api.close()
-    return friend_status == 3 or friend_status == 1  # пользователь в друзьях или же ему отправлена заявка
+async def are_friends(access_token, user_id):
+    api = API(access_token)
+    friend_status = await api.get_api().users.get(user_ids=user_id, fields="friend_status", return_raw_response=True)
+    friend_status = friend_status["response"][0]["friend_status"]
+    await api.close()
+    return friend_status == 1 or friend_status == 3
 
 
-async def is_follower_of_group(follower_subscriptions, public_id):
-    follower_group_subscriptions = follower_subscriptions["groups"]
-    return public_id in follower_group_subscriptions
+async def is_follower_of_group(access_token, group_id):
+    api = API(access_token)
+    is_member = await api.get_api().groups.is_member(group_id=group_id, return_raw_response=True)
+    await api.close()
+    return is_member["response"]
 
 
-async def has_liked_post(access_token, owner_id, post_id):
+async def has_liked_post(access_token, owner_id, post_id):  # owner_id should be negative if group, positive if user
     api = API(access_token)
     is_liked = await api.get_api().likes.is_liked(type="post",
-                                                  owner_id=-owner_id,
+                                                  owner_id=owner_id,
                                                   item_id=post_id,
                                                   return_raw_response=True)
     await api.close()
@@ -31,24 +29,27 @@ async def has_liked_post(access_token, owner_id, post_id):
 async def has_copied_post(access_token, owner_id, post_id):
     api = API(access_token)
     is_liked = await api.get_api().likes.is_liked(type="post",
-                                                  owner_id=-owner_id,
+                                                  owner_id=owner_id,
                                                   item_id=post_id,
                                                   return_raw_response=True)
     await api.close()
     return is_liked["response"]["copied"]
 
-async def test(access_token, user_id):
-    api = API(access_token)
-    vk = api.get_api()
-    friends_list = await vk.friends.get(user_id=265582492, return_raw_response=True)
-    return friends_list
-
 async def main():
-    test123 = await test("d99f968706a51e7c447110532da8b557c02cb433aa8edf4b665b5c1c89b6db0847a8cb0811403963fd88a",
-                         258835738)
-    print(test123)
-    await has_liked_post("785976a4581154ff1c244afd3ea75718cc4d77344fb3373287bf493ba10758ff6543a443342c1f1f04890",
-                         182186143, 3498)
+    test1 = await is_follower_of_group("86a50419c9888124ced2fb1b2f3f5bf9ffc5f97d78adac658495c5e3e227239720bcb99241c65087651ba",
+                                      198971357)
+    print(test1)
+    test2 = await are_friends("86a50419c9888124ced2fb1b2f3f5bf9ffc5f97d78adac658495c5e3e227239720bcb99241c65087651ba",
+                              258835738)
+    print(test2)
+    test3 = await has_liked_post("86a50419c9888124ced2fb1b2f3f5bf9ffc5f97d78adac658495c5e3e227239720bcb99241c65087651ba",
+                                 owner_id=-39485893,
+                                 post_id=15298)
+    print(test3)
+    test4 = await has_copied_post("86a50419c9888124ced2fb1b2f3f5bf9ffc5f97d78adac658495c5e3e227239720bcb99241c65087651ba",
+                                  owner_id=-39485893,
+                                  post_id=15298)
+    print(test4)
 
 
 if __name__ == "__main__":
